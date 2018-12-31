de5b55e531359f914d831dd82502dc1d96415ad3
ChrisManin <tof.giga@hotmail.fr>
2018-12-31 12:09:53 +0100

    Initialisation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MonPremierComponent } from './mon-premier/mon-premier.component';
 import { AppareilComponent } from './appareil/appareil.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from "@angular/common/http";
 import { AuthComponent } from './auth/auth.component';
 import { AppareilViewComponent } from './appareil-view/appareil-view.component';
 import { Routes, RouterModule } from '@angular/router';
@@ -13,13 +14,20 @@ import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
 import { AuthGuard } from './mon-premier/services/auth.guard.service';
 import { AuthService } from './mon-premier/services/auth.service';
 import { AppareilService } from './mon-premier/services/appareil.service';
+import { EditAppareilComponent } from './edit-appareil/edit-appareil.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserService } from './mon-premier/services/user.service';
+import { NewUserComponent } from './new-user/new-user.component';
 
 
 
 const appRoutes: Routes = [
   { path: 'appareils', canActivate: [AuthGuard], component: AppareilViewComponent },
   { path: 'appareils/:id', canActivate: [AuthGuard], component: SingleAppareilComponent },
+  { path: 'edit', canActivate: [AuthGuard], component: EditAppareilComponent },
   { path: 'auth', component: AuthComponent },
+  { path: 'users', component: UserListComponent },
+  { path: 'new-user', component: NewUserComponent },
   { path: '', component: AppareilViewComponent },
   { path: 'not-found', component: FourOhFourComponent },
   { path: '**', redirectTo: '/not-found'}
@@ -33,18 +41,24 @@ const appRoutes: Routes = [
     AuthComponent,
     AppareilViewComponent,
     SingleAppareilComponent,
-    FourOhFourComponent
+    FourOhFourComponent,
+    EditAppareilComponent,
+    UserListComponent,
+    NewUserComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
     RouterModule.forRoot(appRoutes)
   ],
   providers: [
     AppareilService,
     AuthService,
-    AuthGuard
+    AuthGuard,
+    UserService
   ],
   bootstrap: [AppComponent]
 })
--- src/app/mon-premier/services/appareil.service.ts
+++ src/app/mon-premier/services/appareil.service.ts
@@ -1,32 +1,21 @@
 import { Subject } from "rxjs";
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
 
+@Injectable()
 export class AppareilService {
 
     appareilSubject = new Subject<any[]>();
-    
-    private appareils = [
-        {
-            id: 1,
-            name: 'Machine à laver',
-            status: 'éteint'
-        },
-        {
-            id: 2,
-            name: 'Télévision',
-            status: 'allumé'
-        },
-        {
-            id: 3,
-            name: 'Ordinateur',
-            status: 'éteint'
-        }
-    ];
 
-    emitAppareilSubject(){
+    private appareils = [];
+
+    constructor(private httpClient: HttpClient) {}
+
+    emitAppareilSubject() {
         this.appareilSubject.next(this.appareils.slice());
     }
 
-    getAppareilById(id: number){
+    getAppareilById(id: number) {
         const appareil = this.appareils.find(
             (appareilObject) => {
                 return appareilObject.id === id;
@@ -42,22 +31,60 @@ export class AppareilService {
         this.emitAppareilSubject();
     }
 
-    switchOffAll(){
-        for(let appareil of this.appareils){
+    switchOffAll() {
+        for (let appareil of this.appareils) {
             appareil.status = 'éteint';
         }
         this.emitAppareilSubject();
     }
 
-    switchOnOne(index: number){
+    switchOnOne(index: number) {
         this.appareils[index].status = 'allumé';
         this.emitAppareilSubject();
     }
 
-    switchOffOne(index: number){
+    switchOffOne(index: number) {
         this.appareils[index].status = 'éteint';
         this.emitAppareilSubject();
     }
 
-    
+    addAppareil(name: string, status: string) {
+        const appareilObject = {
+            id: 0,
+            name: '',
+            status: ''
+        };
+        appareilObject.name = name;
+        appareilObject.status = status;
+        appareilObject.id = this.appareils[(this.appareils.length - 1)].id + 1;
+        this.appareils.push(appareilObject);
+        this.emitAppareilSubject();
+    }
+
+    saveAppareilToServer() {
+        this.httpClient
+        .put('https://http-client-demo-c5437.firebaseio.com/appareils.json', this.appareils)
+        .subscribe(
+            () => {
+                console.log('Enregistrement terminé !');
+            },
+            (error) => {
+                console.log('Erreur de sauvegarde ! ' + error);
+            }
+        );
+    }
+
+    getAppareilsFromServer(){
+        this.httpClient
+        .get<any[]>('https://http-client-demo-c5437.firebaseio.com/appareils.json')
+        .subscribe(
+            (response) => {
+                this.appareils = response;
+                this.emitAppareilSubject();
+            },
+            (error) => {
+                console.log('Erreur de chargement ! '+ error);
+            }
+        );
+    }
 }
